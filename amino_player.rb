a9fe439354175c27e054a8a299ca522d1f887088
Dominik Kundel <dominik.kundel@me.com>
2013-05-12 10:55:41 +0200

    included process for creating the file

---
+++ amino_player.rb
@@ -0,0 +1,68 @@
+class AminoPlayer
+  def readMappings(filestring)
+    @translation = Hash.new
+    filestring.each_line do |line|
+      linearr = line.split(" - ")
+      key = linearr[0]
+      tone = linearr[1].strip!.to_i
+      @translation[key] = tone
+    end
+  end
+
+  def createDataArray(filestring)
+    @tones = Array.new
+    filestring.each_line do |line|
+      linearr = line.scan(/.{1,3}/)
+      linearr.each do |s|
+        @tones << @map[s]
+      end
+    end
+    @tones
+  end
+
+  def createSequence()
+    @seq = Sequence.new()
+    # Create a first track for the sequence. This holds tempo events and stuff
+    # like that.
+    track = Track.new(seq)
+    seq.tracks << track
+    track.events << Tempo.new(Tempo.bpm_to_mpq(120))
+    track.events << MetaEvent.new(META_SEQ_NAME, 'Sequence Name')
+
+    # Create a track to hold the notes. Add it to the sequence.
+    track = Track.new(seq)
+    seq.tracks << track
+
+    # Give the track a name and an instrument name (optional).
+    track.name = 'AminoBeats'
+    track.instrument = GM_PATCH_NAMES[0]
+
+    # Add a volume controller event (optional).
+    track.events << Controller.new(0, CC_VOLUME, 127)
+
+    # Add events to the track: a major scale. Arguments for note on and note off
+    # constructors are channel, note, velocity, and delta_time. Channel numbers
+    # start at zero. We use the new Sequence#note_to_delta method to get the
+    # delta time length of a single quarter note.
+    track.events << ProgramChange.new(0, 1, 0)
+    quarter_note_length = seq.note_to_delta('quarter')
+    getData(getMap()).each do |offset|
+      if offset == -1
+        offset = 0
+      end 
+        track.events << NoteOn.new(0, offset, 127, 0)
+        track.events << NoteOff.new(0, offset, 127, quarter_note_length)
+    end
+
+    # Calling recalc_times is not necessary, because that only sets the events'
+    # start times, which are not written out to the MIDI file. The delta times are
+    # what get written out.
+
+    # track.recalc_times
+
+    File.open('translation.mid', 'wb') { |file| seq.write(file) }
+
+    %x(open translation.mid)
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,36 @@
+require './aminobeats.rb'
+require './amino_player.rb'
+
+player = AminoPlayer.new
+
+#Sample file explaining how to use the aminobeats ui api
+
+#We require a function that is called whenever the user opens
+#or edits the mappings of amionacids to notes. This function
+#will recieive as it's first argument a string containing the
+#mappings
+
+editMappings = Proc.new do |filestring|
+  player.readMappings(filestring)
+end
+
+
+#Second we require a function to be executed when the user
+#writes an input sequence and clicks play. The function 
+#will receive as it's first argument the string containing
+#the sequence
+
+playSequence = Proc.new do |seq|
+  player.createDataArray(seq)
+  player.createSequence()
+end
+
+#Finally we require a function to be called when the
+#user clicks the stop button. It takes no arguments.
+stopPlay = Proc.new do
+#Indicate that music was stopped
+  printf "Stopped"
+end
+
+#Call the function to initialize the gui with your callbacks
+initApp(editMappings,playSequence,stopPlay)
