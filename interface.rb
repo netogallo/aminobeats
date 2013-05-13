7344cced8ede0d575c835cf59590318477999675
Ernesto Rodriguez <neto@netowork.me>
2013-05-13 23:07:01 +0200

    good shit

--- interface.rb
+++ interface.rb
@@ -1,7 +1,7 @@
 =begin
 ** Form generated from reading ui file 'interface.ui'
 **
-** Created: Mon May 13 14:28:40 2013
+** Created: Mon May 13 23:01:49 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
@@ -12,15 +12,14 @@ require 'Qt4'
 class Ui_MainWindow
     attr_reader :aboutButton
     attr_reader :centralwidget
-    attr_reader :verticalLayout_4
+    attr_reader :verticalLayout_2
     attr_reader :horizontalLayout_2
     attr_reader :logo
     attr_reader :label_2
-    attr_reader :plot
-    attr_reader :horizontalLayout_4
-    attr_reader :verticalLayout_2
+    attr_reader :horizontalLayout_5
     attr_reader :horizontalLayout
     attr_reader :verticalLayout
+    attr_reader :plot
     attr_reader :sequenceEdit
     attr_reader :aminosList
     attr_reader :horizontalLayout_3
@@ -35,8 +34,6 @@ class Ui_MainWindow
     attr_reader :verticalLayout_3
     attr_reader :notesLayout
     attr_reader :verticalSpacer
-    attr_reader :menubar
-    attr_reader :menuHelp
     attr_reader :statusbar
 
     def setupUi(mainWindow)
@@ -44,13 +41,20 @@ class Ui_MainWindow
         mainWindow.objectName = "mainWindow"
     end
     mainWindow.resize(800, 921)
-    mainWindow.styleSheet = ""
+    mainWindow.styleSheet = "#centralwidget{\n" \
+"	background-image:url(dark-subtle-patterns/pattern4.jpg);\n" \
+"}\n" \
+"\n" \
+"#aminosList, #sequenceEdit{\n" \
+"\n" \
+"	background:#333;\n" \
+"}"
     @aboutButton = Qt::Action.new(mainWindow)
     @aboutButton.objectName = "aboutButton"
     @centralwidget = Qt::Widget.new(mainWindow)
     @centralwidget.objectName = "centralwidget"
-    @verticalLayout_4 = Qt::VBoxLayout.new(@centralwidget)
-    @verticalLayout_4.objectName = "verticalLayout_4"
+    @verticalLayout_2 = Qt::VBoxLayout.new(@centralwidget)
+    @verticalLayout_2.objectName = "verticalLayout_2"
     @horizontalLayout_2 = Qt::HBoxLayout.new()
     @horizontalLayout_2.objectName = "horizontalLayout_2"
     @logo = Qt::Label.new(@centralwidget)
@@ -62,43 +66,49 @@ class Ui_MainWindow
     @label_2 = Qt::Label.new(@centralwidget)
     @label_2.objectName = "label_2"
     @font = Qt::Font.new
-    @font.family = "Utopia"
+    @font.family = "Courier 10 Pitch"
     @font.pointSize = 36
-    @font.bold = true
-    @font.italic = true
-    @font.weight = 75
+    @font.bold = false
+    @font.italic = false
+    @font.weight = 50
     @label_2.font = @font
+    @label_2.styleSheet = "#label_2 {\n" \
+"	color: #fff;\n" \
+"	font: 36pt \"Courier 10 Pitch\";\n" \
+"}"
 
     @horizontalLayout_2.addWidget(@label_2)
 
 
-    @verticalLayout_4.addLayout(@horizontalLayout_2)
+    @verticalLayout_2.addLayout(@horizontalLayout_2)
 
-    @plot = Qt::Label.new(@centralwidget)
-    @plot.objectName = "plot"
-    @plot.minimumSize = Qt::Size.new(0, 200)
+    @horizontalLayout_5 = Qt::HBoxLayout.new()
+    @horizontalLayout_5.objectName = "horizontalLayout_5"
 
-    @verticalLayout_4.addWidget(@plot)
+    @verticalLayout_2.addLayout(@horizontalLayout_5)
 
-    @horizontalLayout_4 = Qt::HBoxLayout.new()
-    @horizontalLayout_4.objectName = "horizontalLayout_4"
-    @verticalLayout_2 = Qt::VBoxLayout.new()
-    @verticalLayout_2.objectName = "verticalLayout_2"
     @horizontalLayout = Qt::HBoxLayout.new()
     @horizontalLayout.objectName = "horizontalLayout"
     @verticalLayout = Qt::VBoxLayout.new()
     @verticalLayout.objectName = "verticalLayout"
+    @plot = Qt::Label.new(@centralwidget)
+    @plot.objectName = "plot"
+    @plot.minimumSize = Qt::Size.new(0, 200)
+
+    @verticalLayout.addWidget(@plot)
+
     @sequenceEdit = Qt::TextEdit.new(@centralwidget)
     @sequenceEdit.objectName = "sequenceEdit"
+    @sequenceEdit.styleSheet = "#sequenceEdit {\n" \
+"	color: #fff;\n" \
+"}"
 
     @verticalLayout.addWidget(@sequenceEdit)
 
     @aminosList = Qt::Label.new(@centralwidget)
     @aminosList.objectName = "aminosList"
     @aminosList.minimumSize = Qt::Size.new(0, 40)
-    @aminosList.styleSheet = "#aminosList{\n" \
-"	background-color: #FFFFFF;\n" \
-"}"
+    @aminosList.styleSheet = ""
     @aminosList.textFormat = Qt::RichText
     @aminosList.wordWrap = true
 
@@ -149,14 +159,11 @@ class Ui_MainWindow
 
     @horizontalLayout.addLayout(@verticalLayout)
 
-
-    @verticalLayout_2.addLayout(@horizontalLayout)
-
-
-    @horizontalLayout_4.addLayout(@verticalLayout_2)
-
     @notes = Qt::Frame.new(@centralwidget)
     @notes.objectName = "notes"
+    @notes.styleSheet = "#notes QLabel{\n" \
+"	color:#fff;\n" \
+"}"
     @notes.frameShape = Qt::Frame::StyledPanel
     @notes.frameShadow = Qt::Frame::Raised
     @verticalLayout_3 = Qt::VBoxLayout.new(@notes)
@@ -167,28 +174,19 @@ class Ui_MainWindow
     @verticalLayout_3.addLayout(@notesLayout)
 
 
-    @horizontalLayout_4.addWidget(@notes)
+    @horizontalLayout.addWidget(@notes)
 
 
-    @verticalLayout_4.addLayout(@horizontalLayout_4)
+    @verticalLayout_2.addLayout(@horizontalLayout)
 
     @verticalSpacer = Qt::SpacerItem.new(20, 115, Qt::SizePolicy::Minimum, Qt::SizePolicy::Expanding)
 
-    @verticalLayout_4.addItem(@verticalSpacer)
+    @verticalLayout_2.addItem(@verticalSpacer)
 
     mainWindow.centralWidget = @centralwidget
-    @menubar = Qt::MenuBar.new(mainWindow)
-    @menubar.objectName = "menubar"
-    @menubar.geometry = Qt::Rect.new(0, 0, 800, 21)
-    @menuHelp = Qt::Menu.new(@menubar)
-    @menuHelp.objectName = "menuHelp"
-    mainWindow.setMenuBar(@menubar)
     @statusbar = Qt::StatusBar.new(mainWindow)
     @statusbar.objectName = "statusbar"
     mainWindow.statusBar = @statusbar
-
-    @menubar.addAction(@menuHelp.menuAction())
-    @menuHelp.addAction(@aboutButton)
 
     retranslateUi(mainWindow)
 
@@ -205,6 +203,11 @@ class Ui_MainWindow
     @logo.text = ''
     @label_2.text = Qt::Application.translate("MainWindow", "Amino Sonic", nil, Qt::Application::UnicodeUTF8)
     @plot.text = ''
+    @sequenceEdit.html = Qt::Application.translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n" \
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n" \
+"p, li { white-space: pre-wrap; }\n" \
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:10pt; font-weight:400; font-style:normal;\">\n" \
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nil, Qt::Application::UnicodeUTF8)
     @aminosList.text = ''
     @divisionCombo.insertItems(0, [Qt::Application.translate("MainWindow", "Greater", nil, Qt::Application::UnicodeUTF8),
         Qt::Application.translate("MainWindow", "Smaller", nil, Qt::Application::UnicodeUTF8)])
@@ -212,7 +215,6 @@ class Ui_MainWindow
     @plotDistributionButton.text = Qt::Application.translate("MainWindow", "Plot Distribution", nil, Qt::Application::UnicodeUTF8)
     @playButton.text = Qt::Application.translate("MainWindow", "Play", nil, Qt::Application::UnicodeUTF8)
     @stopButton.text = Qt::Application.translate("MainWindow", "Stop", nil, Qt::Application::UnicodeUTF8)
-    @menuHelp.title = Qt::Application.translate("MainWindow", "Help", nil, Qt::Application::UnicodeUTF8)
     end # retranslateUi
 
     def retranslate_ui(mainWindow)
